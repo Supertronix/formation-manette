d6243a0a48872bfa6afc556d0804b4d26c446eb5
nadineducegep <nadineducegep@gmail.com>
2018-01-30 22:21:20 -0500

    Axes de manette injectees dans les roues

--- src/org/usfirst/frc/equipe5910/robot/RobotControleur.java
+++ src/org/usfirst/frc/equipe5910/robot/RobotControleur.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.equipe5910.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.VictorSP;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -15,8 +16,23 @@ public class RobotControleur extends IterativeRobot {
 	public static final int MANETTE_CONDUITE_PRINCIPALE = 0;
 	public static final int MANETTE_ACCESSOIRE = 1;	
 	
+	// PWM Outputs
+	public static final int ROUE_AVANT_GAUCHE = 10; // SP 1
+	public static final int ROUE_ARRIERE_GAUCHE = 14; // SP 2
+	public static final int ROUE_AVANT_DROIT= 13; // SP 3
+	public static final int ROUE_ARRIERE_DROIT = 17; // SP 4
+	
+	public static final boolean INVERSION_ROUE_AVANT_GAUCHE = true;
+	public static final boolean INVERSION_ROUE_ARRIERE_GAUCHE = true;
+	public static final boolean INVERSION_ROUE_AVANT_DROIT = false;
+	public static final boolean INVERSION_ROUE_ARRIERE_DROIT= false;
+	
     private Joystick manetteConduitePrincipale;
     private Joystick manetteAccessoire;	
+    VictorSP roueAvantGauche; 
+	VictorSP roueArriereGauche; 
+	VictorSP roueAvantDroite; 
+	VictorSP roueArriereDroite; 
 	
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -28,6 +44,16 @@ public class RobotControleur extends IterativeRobot {
 		System.out.println("robotInit()");
     	manetteConduitePrincipale = new Joystick(MANETTE_CONDUITE_PRINCIPALE);
     	manetteAccessoire = new Joystick(MANETTE_ACCESSOIRE);
+    	
+		roueAvantGauche = new VictorSP(ROUE_AVANT_GAUCHE);
+		roueArriereGauche = new VictorSP(ROUE_ARRIERE_GAUCHE);
+		roueAvantDroite = new VictorSP(ROUE_AVANT_DROIT);
+		roueArriereDroite = new VictorSP(ROUE_ARRIERE_DROIT);
+		 	
+		roueAvantGauche.setInverted(INVERSION_ROUE_AVANT_GAUCHE); // TRUE
+		roueArriereGauche.setInverted(INVERSION_ROUE_ARRIERE_GAUCHE); // TRUE
+		roueAvantDroite.setInverted(INVERSION_ROUE_AVANT_DROIT);
+		roueArriereDroite.setInverted(INVERSION_ROUE_ARRIERE_DROIT);
 	}
 
 	/**
@@ -60,6 +86,7 @@ public class RobotControleur extends IterativeRobot {
 	public static final int CONDUITE_Y_GAUCHE = 1;
 	public static final int CONDUITE_X_DROITE = 4;
 	public static final int CONDUITE_Y_DROITE = 5;
+
 	
 	/**
 	 * This function is called periodically during operator control
@@ -70,19 +97,22 @@ public class RobotControleur extends IterativeRobot {
 	    //public double getConduiteGaucheX() {
 	    double xGauche = manetteConduitePrincipale.getRawAxis(CONDUITE_X_GAUCHE);
 	    System.out.println("X gauche : " + xGauche);
+	    this.roueAvantGauche.set(xGauche);
 	    
 	    //public double getConduiteGaucheY() {
 	    double yGauche = -manetteConduitePrincipale.getRawAxis(CONDUITE_Y_GAUCHE); 
 	    System.out.println("Y gauche : " + yGauche);
+	    this.roueArriereGauche.set(yGauche);
 	    
 	    //public double getConduiteDroiteX() {
 	    double xDroit = manetteConduitePrincipale.getRawAxis(CONDUITE_X_DROITE);
 	    System.out.println("X droit : " + xDroit);
+	    this.roueAvantDroite.set(xDroit);
 	      
 	    //public double getConduiteDroiteY() {
 	    double yDroit = -manetteConduitePrincipale.getRawAxis(CONDUITE_Y_DROITE);
 	    System.out.println("Y droit : " + yDroit);
-	  		
+	    this.roueArriereDroite.set(yDroit);	  		
 	}
 
 	/**
